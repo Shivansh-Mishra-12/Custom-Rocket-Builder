7455f09e2a8a43c216dccafad1f8396fa5da6378
Shivansh Mishra <s70743553@gmail.com>
2026-06-26 10:02:24 +0530

    Changed the rocket model with a new one, and did some other  necessary changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,6 +4,8 @@ import Model from '../components/Model'
 import { BoxGeometry, TextureLoader } from 'three'
 import { useRef } from 'react'
 import { useLoader } from '@react-three/fiber'
+import Boosters from './Boosters'
+import FalconHeavy from './FalconHeavy'
 const Container = () => {
     const nMap = useLoader(TextureLoader, '../textures/coast_land_rocks_01_diff_2k.jpg')
     const selected_part_Ref = useRef()
@@ -24,8 +26,9 @@ const Container = () => {
                         selected_part_Ref.current = null
                     }
                 }}>
-                    <Model position={[0, -0.3, 0]} scale={.003} Select_Handle={Select_Handle} />
-                    <StrongBack position={[0.63, -.8, 0]} scale={.12} Select_Handle={Select_Handle} />
+                    <FalconHeavy position={[0, .7, 1.3]}  scale={.009} Select_Handle={Select_Handle} />
+                    <StrongBack position={[0,0,-5]} rotation={[0,Math.PI/2,0]} scale={.12} Select_Handle={Select_Handle} />
+                    {/* <Boosters position={[0,0,-.21]} scale={0.02} rotation={[-Math.PI/2,0,0]}  Select_Handle={Select_Handle}/> */}
                 </group>
                 <group rotation={[Math.PI / 2, 0, 0]} position={[0, -1, 0]} >
                     <mesh >
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,20 +4,6 @@ import * as THREE from 'three'
 const Model = ({Select_Handle, ...props}) => {
     const modelRef = useRef()
     const { scene } = useGLTF('../models/falcon_9_spacex_rocket.glb')
-    const fairing = scene.getObjectByName('Cylinder001_fairing_0')
-    // const selected_part_Ref = useRef()
-    if (fairing && fairing.parent) {
-        fairing.visible = true
-    }
-    // const Select_Handle = (obj) => {
-    //     if (selected_part_Ref.current) {
-    //         selected_part_Ref.current.material.emissive?.set(0x000000)
-    //     }
-
-    //     obj.material.emissive?.set(0x00ff00)
-    //     selected_part_Ref.current = obj
-    // }
-
     return (
         <group ref={modelRef} {...props} >
             <primitive
